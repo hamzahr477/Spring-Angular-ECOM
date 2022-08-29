ec14dc3f4ef3e36a76ed800847f553e231266993
hamzahayar1@gmail.com <HAMZA@hambh1>
2022-08-29 06:40:15 +0100

    Get All Orders GET: orders/

--- src/main/java/lu/atozdigital/api/controller/OrderController.java
+++ src/main/java/lu/atozdigital/api/controller/OrderController.java
@@ -1,7 +1,12 @@
 package lu.atozdigital.api.controller;
 
+import lu.atozdigital.api.service.CommandeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/orders")
 @Validated
 public class OrderController {
+    /**
+     * Declaration of commande service with injection
+     */
+    @Autowired
+    CommandeService commandeService;
 
+    /**
+     * Get All Orders saved in database
+     * @return a Response Entity with List of Orders
+     */
+    @GetMapping("")
+    public ResponseEntity<?> getAllOrder(){
+        return new ResponseEntity<>(commandeService.getAllCommandes(), HttpStatus.OK);
+    }
 }
