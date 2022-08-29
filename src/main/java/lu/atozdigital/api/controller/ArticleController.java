7954157536430648f501502c3e0ba062dfd43837
hamzahayar1@gmail.com <HAMZA@hambh1>
2022-08-29 06:29:30 +0100

    Get All Product endpoint artilcles/

--- src/main/java/lu/atozdigital/api/controller/ArticleController.java
+++ src/main/java/lu/atozdigital/api/controller/ArticleController.java
@@ -1,7 +1,12 @@
 package lu.atozdigital.api.controller;
 
+import lu.atozdigital.api.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/articles")
 @Validated
 public class ArticleController {
+    /**
+     * productService a Service contain all method form product management
+     */
+    @Autowired
+    ProductService productService;
+
+    /**
+     * Get All product saved in database
+     * @return Response Entity contain List of Products
+     */
+    @GetMapping("")
+    ResponseEntity<?> getAllProduct(){
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
+    }
 }
